244b0c3c99793b9cb77011569d547ad6f7791a67
radfly <radflyone@gmail.com>
2020-01-02 19:27:53 -0500

    Getting this!!!!

--- Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,22 +17,40 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (26) Create an EditText variable called mSearchBoxEditText
+    EditText mSearchBoxEditText;
 
-    // TODO (27) Create a TextView variable called mUrlDisplayTextView
-    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    TextView mUrlDisplayTextView;
+
+    TextView mSearchResultsTextView;
+
+
+
+    // dTODO (26) Create an EditText variable called mSearchBoxEditText
+
+    // dODO (27) Create a TextView variable called mUrlDisplayTextView
+    // dTODO (28) Create a TextView variable called mSearchResultsTextView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (29) Use findViewById to get a reference to mSearchBoxEditText
+        // dTODO (29) Use findViewById to get a reference to mSearchBoxEditText
 
-        // TODO (30) Use findViewById to get a reference to mUrlDisplayTextView
-        // TODO (31) Use findViewById to get a reference to mSearchResultsTextView
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+
+
+        // dTODO (30) Use findViewById to get a reference to mUrlDisplayTextView
+
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+
+        // dTODO (31) Use findViewById to get a reference to mSearchResultsTextView
+
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 }
--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,8 +17,12 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,22 +44,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
-    // TODO (2) Create a menu xml called 'main.xml' in the res->menu folder
-    // TODO (3) Add one menu item to your menu
-    // TODO (4) Give the menu item an id of @+id/action_search
-    // TODO (5) Set the orderInCategory to 1
-    // TODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
-    // TODO (7) Set the title to the search string ("Search") from strings.xml
+    // dTODO (2) Create a menu xml called 'main.xml' in the res->menu folder
+    // dTODO (3) Add one menu item to your menu
+    // dTODO (4) Give the menu item an id of @+id/action_search
+    // dTODO (5) Set the orderInCategory to 1
+    // dTODO (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
+    // dTODO (7) Set the title to the search string ("Search") from strings.xml
     // Do 2 - 7 in main.xml ///////////////////////////////////////////////////////////////////////
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
 
-    // TODO (8) Override onCreateOptionsMenu
-    // TODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
-    // TODO (10) Return true to display your menu
+        MenuItem searchItem = menu.findItem(R.id.action_search);
 
-    // TODO (11) Override onOptionsItemSelected
-    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+        return true;
+    }
+
+
+    // dTODO (8) Override onCreateOptionsMenu
+    // dTODO (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
+    // dTODO (10) Return true to display your menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                Toast.makeText(getApplicationContext(), "Search clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        // dTODO (11) Override onOptionsItemSelected
+        // dTODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
+        // dTODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
+        // dTODO (14) Don't forgot to call .show() on your Toast
+        // dTODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+    }
 }
